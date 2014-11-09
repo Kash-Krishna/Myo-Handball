5939f120250d69999bc1e5e5f5411033953423e4
Elias Bravo <ebrav005@ucr.edu>
2014-11-08 21:27:53 -0800

    MYOII

---
+++ MYOII/Assets/Movement.js
@@ -0,0 +1,50 @@
+﻿#pragma strict
+@script RequireComponent( Rigidbody )
+
+var objSphere : GameObject;
+var xAlt = 5;
+var yAlt = 2;
+var zAlt = 4;
+
+function Start () {
+}
+
+function Update () {
+	/*if(Input.GetKey ("space"))
+	{
+		objSphere.transform.position.x += 5;
+	}*/
+
+	objSphere.rigidbody.AddForce(xAlt,yAlt,zAlt);
+}
+
+function OnCollisionEnter (col : Collision) {
+	if(col.gameObject.name == "PLeft")
+	{
+		xAlt = xAlt * -1;
+		//objSphere.rigidbody.AddForce(alternate * -1,0,0);
+		//yAlt = yAlt * -1;
+	}
+	
+	if(col.gameObject.name == "PRight")
+	{
+		xAlt = xAlt * -1;
+		//objSphere.rigidbody.AddForce(alternate * -1,0,0);
+		//yAlt = yAlt * -1;
+	}
+	
+	if(col.gameObject.name == "Wall")
+	{
+		zAlt = zAlt * -1;
+		//yAlt = yAlt * -1;
+	}
+	
+	if(col.gameObject.name == "Wall2")
+	{
+		zAlt = zAlt * -1;
+		//yAlt = yAlt * -1;
+	}
+}
+
+
+
